07320478207e62cc1f9a69cd70ae05647240ca3d
Pratham <prathamsharmafaangm@gmail.com>
2022-05-14 01:37:18 +0530

    Number Guessing Game finalized

--- #2 Intermediate/Chrome Extension/index.js
+++ #2 Intermediate/Chrome Extension/index.js
@@ -1,6 +1,12 @@
 
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
-let inputBtn = document.getElementById("input-btn");
+/*The main differnce between a const and a let variable is
+that value for a const variable cannot be reassigned*/
+
 inputBtn.addEventListener("click",function(){
-  console.log("Button Clicked Again")
+  myLeads.push("www.awesomelead.com")
+  console.log(myLeads);
 })
--- #2 Intermediate/Number Guessing Game/index.js
+++ #2 Intermediate/Number Guessing Game/index.js
@@ -15,15 +15,15 @@ function generateRandomNum(){
 }
 
 function checkNumber(){
-  if (inputEl === randomNumber1 )
+  if (inputEl.value == randomNumber1 )
   {
     message = "Bingo! You've guessed it right!";
   }
-  else if (inputEl > randomNumber1)
+  else if (inputEl.value  > randomNumber1)
   {
     message = "Hint : The number is actually lesser than this";
   }
-  else if (inputEl < randomNumber1)
+  else if (inputEl.value  < randomNumber1)
   {
     message = "Hint : The number is actually greater than this";
   }
