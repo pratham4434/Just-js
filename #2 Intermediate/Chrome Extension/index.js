a368bf96217f4436a1a3193dfd26ca3d198786ca
Pratham <prathamsharmafaangm@gmail.com>
2022-05-15 12:43:58 +0530

    final deployment

--- #2 Intermediate/Chrome Extension/index.js
+++ #2 Intermediate/Chrome Extension/index.js
@@ -1,34 +1,75 @@
 
-let myLeads = [];
+let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-console.log(ulEl);
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-/*The main difference between a const and a let variable is
-that value for a const variable cannot be reassigned*/
+// // 1. Turn the myLeads string into an array.
+// myLeads = JSON.parse(myLeads)
+// // 2. Push a new value to the array.
+// myLeads.push("www.lead2.com")
+// // 3. Turn the array into string again.
+// myLeads = JSON.stringify(myLeads)
+// // 4. Console.log the string using typeof to verify that it's a string.
+// console.log(typeof myLeads)
 
-inputBtn.addEventListener("click",function(){
-  myLeads.push(inputEl.value)
-  renderLeads();
-  inputEl.value = "";
+tabBtn.addEventListener("click", function(){
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    localStorage.setItem( "myLeads", JSON.stringify(myLeads) )
+    myLeads.push(tabs[0].url)
+    render(myLeads)
+  })
+
 })
 
-function renderLeads(){
+
+deleteBtn.addEventListener("dblclick",function(){
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+
+if (leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+function render(leads){
   let listItems = ""
   // forloop that logs out all the data in myLeads array.
   // Here, we call the i as the iterator.
-  for(let i = 0; i < myLeads.length; i++ ){
+  for(let i = 0; i < leads.length; i++ ){
   //  listItems += "<li><a  target = '_blank' href ='" + myLeads[i] + "'>"  + myLeads[i] + "</a></li>";
   listItems += `
                 <li>
-                  <a target = '_blank' href ='${myLeads[i]}'>
-                      ${myLeads[i]}
+                  <a target = '_blank' href ='${leads[i]}'>
+                      ${leads[i]}
                   </a>
                 </li>
                 `
-
-  console.log(listItems)
   }
   ulEl.innerHTML = listItems;
 }
+/* The main difference between a const and a let variable is
+that value for a const variable cannot be reassigned*/
+
+/* The addEventListener() method attches an event handler to an
+ element without overwriting existing  event handlers. You can
+ add many event handlers of the same type to one element, eg.
+ two click events*/
+
+inputBtn.addEventListener("click",function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads);
+
+  console.log( localStorage.getItem("myLeads"))
+})
