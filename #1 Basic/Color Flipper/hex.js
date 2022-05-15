46e6768f21f6408d96ee4e09667007ebb033fa70
Pratham <prathamsharmafaangm@gmail.com>
2022-05-15 14:47:52 +0530

    Hex Color Flipper

--- #1 Basic/Color Flipper/hex.js
+++ #1 Basic/Color Flipper/hex.js
@@ -1,1 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    let hexColor = '#';
+    for( let i = 0; i < 6; i++ ){
+        let newVal = Math.floor(Math.random()*16);
+        hexColor += hex[newVal];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
