1d0f3820307e2eddd85de0034e168b6e4e2f8cd1
Pratham <prathamsharmafaangm@gmail.com>
2022-05-18 00:34:24 +0530

    async await

--- #3 Advanced/async.js
+++ #3 Advanced/async.js
@@ -1,26 +1,45 @@
-// synchronous
+/*
+//// synchronous ////
 
 console.log(" I ");
-
 console.log(" eat ");
-
 console.log(" ice cream ");
-
 console.log(" with a ");
-
 console.log(" spoon ");
 
-// asynchronous
+
+//// asynchronous ////
 
 console.log(" I ");
-
 console.log(" eat ");
-
 setTimeout(() => {
     console.log(" ice cream ");
 }, 4000);
-
 console.log(" with a ");
-
 console.log(" spoon ");
 
+*/
+
+// we are going to make an object
+let stocks = {
+Fruits : ["strawberry","grapes","banana","apple"],
+liquid : ["water", "ice"],
+holder : ["cone","cup","stick"],
+toppings : ["chocolate", "peanuts"],
+};
+
+let order = ( Fruit_name, call_production) => {
+    setTimeout(()=>{
+console.log(`${stocks.Fruits[Fruit_name]}`);
+    },2000);
+
+    
+    call_production();
+};
+
+let production = () => {
+    
+};
+
+
+order("0",production)
---
+++ #3 Advanced/functions.js
@@ -0,0 +1,31 @@
+function sum (a, b) {
+    return a + b
+}
+
+let sum2 = (a,b) => {
+    return a+b;
+}
+
+
+
+function isPositive(number) {
+    return number >= 0
+}
+
+let isPositive2 = (number) => {
+    return number >= 0
+}
+
+
+
+function randomNumber(){
+    return Math.random
+}
+
+let randomNumber2 = () => {
+    return Math.random;
+}
+
+document.addEventListener('click', function(){
+    console.log('click')
+})
