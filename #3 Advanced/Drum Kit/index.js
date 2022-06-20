74bfc4ac217d61ea7a288df5386be4b775151b8f
Pratham <prathamsharmafaangm@gmail.com>
2022-06-21 02:10:44 +0530

    Drum Kit initiated

---
+++ #3 Advanced/Drum Kit/index.js
@@ -0,0 +1,15 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++){
+    let anyButton = document.querySelectorAll("button")[i]
+    anyButton.addEventListener("click", function (){
+       
+
+    })
+}
+
+
+
+
+// let audio = new Audio ("sounds/tom-1.mp3");
+// audio.play();
